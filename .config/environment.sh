674420ef15122b1abe5606856613bcb937a13bd2
Patrik Gustafsson <patrik.gustafsson@purplescout.se>
2015-04-21 12:05:06 +0200

    Environment config

---
+++ .config/environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wmname LG3D 
+
+export JAVA_HOME=/usr/java/jdk1.7.0_71/
+
+export PATH=$PATH:~/bin:~/opt/netbeans-8.0.1/bin/
+
+export MAVEN_OPTS="-Dlog4j.configuration=file:/home/pvn/thecube/log4j"
+
+
+
