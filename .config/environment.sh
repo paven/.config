3f440e2f757167f44d0de01aba4611e5c429bd87
Patrik Gustafsson <patrik.gustafsson@purplescout.se>
2015-09-11 11:50:13 +0200

    Configuring idea

--- .config/environment.sh
+++ .config/environment.sh
@@ -3,7 +3,7 @@ wmname LG3D
 
 export JAVA_HOME=/usr/java/jdk1.7.0_71/
 
-export PATH=$PATH:~/bin:~/opt/netbeans-8.0.1/bin/
+export PATH=$PATH:~/bin:~/opt/netbeans-8.0.1/bin/:~/opt/idea-IU-141.1532.4/bin
 
 export MAVEN_OPTS="-Dlog4j.configuration=file:/home/pvn/thecube/log4j"
 
